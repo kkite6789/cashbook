9d40c9db40225e53333b77abb1f43c9ac3d785b7
GDJ26 <GDJ26@DESKTOP-MPDLFCK>
2020-05-12 16:15:23 +0900

    1.member 추가 2.addMember html 추가 3.memberController 수정

---
+++ cashbook/src/main/java/com/gdu/cashbook/controller/MemberController.java
@@ -0,0 +1,21 @@
+package com.gdu.cashbook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.gdu.cashbook.vo.Member;
+
+@Controller
+public class MemberController {
+	@GetMapping("/addMember")
+	public String addMember() {
+		
+		return "addMember";
+	}
+	@PostMapping("/addMember") 
+	public String addMember(Member member) {//commender
+		System.out.println(member.toString());
+		return "redirect:/index";
+	}
+}
